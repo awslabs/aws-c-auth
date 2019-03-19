ee4eda8bbf10dac895786f874346d5c7f60ee16c
Bret Ambrose <bambrose@amazon.com>
2019-03-19 14:46:06 -0700

    Format with our controls not the default

--- tests/credentials_tests.c
+++ tests/credentials_tests.c
@@ -21,59 +21,47 @@
 
 AWS_STATIC_STRING_FROM_LITERAL(s_access_key_id_test_value, "My Access Key");
 AWS_STATIC_STRING_FROM_LITERAL(s_secret_access_key_test_value, "SekritKey");
-AWS_STATIC_STRING_FROM_LITERAL(s_session_token_test_value,
-                               "Some Session Token");
+AWS_STATIC_STRING_FROM_LITERAL(s_session_token_test_value, "Some Session Token");
 
-static int s_credentials_create_destroy_test(struct aws_allocator *allocator,
-                                             void *ctx) {
-  (void)ctx;
+static int s_credentials_create_destroy_test(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
 
-  struct aws_credentials *credentials = aws_credentials_new(
-      allocator, s_access_key_id_test_value, s_secret_access_key_test_value,
-      s_session_token_test_value);
+    struct aws_credentials *credentials = aws_credentials_new(
+        allocator, s_access_key_id_test_value, s_secret_access_key_test_value, s_session_token_test_value);
 
-  ASSERT_TRUE(aws_string_compare(credentials->access_key_id,
-                                 s_access_key_id_test_value) == 0);
-  ASSERT_TRUE(aws_string_compare(credentials->secret_access_key,
-                                 s_secret_access_key_test_value) == 0);
-  ASSERT_TRUE(aws_string_compare(credentials->session_token,
-                                 s_session_token_test_value) == 0);
+    ASSERT_TRUE(aws_string_compare(credentials->access_key_id, s_access_key_id_test_value) == 0);
+    ASSERT_TRUE(aws_string_compare(credentials->secret_access_key, s_secret_access_key_test_value) == 0);
+    ASSERT_TRUE(aws_string_compare(credentials->session_token, s_session_token_test_value) == 0);
 
-  aws_credentials_destroy(credentials);
+    aws_credentials_destroy(credentials);
 
-  return 0;
+    return 0;
 }
 
-AWS_TEST_CASE(credentials_create_destroy_test,
-              s_credentials_create_destroy_test);
+AWS_TEST_CASE(credentials_create_destroy_test, s_credentials_create_destroy_test);
 
 static int s_credentials_copy_test(struct aws_allocator *allocator, void *ctx) {
-  (void)ctx;
+    (void)ctx;
 
-  struct aws_credentials *source = aws_credentials_new(
-      allocator, s_access_key_id_test_value, s_secret_access_key_test_value,
-      s_session_token_test_value);
+    struct aws_credentials *source = aws_credentials_new(
+        allocator, s_access_key_id_test_value, s_secret_access_key_test_value, s_session_token_test_value);
 
-  struct aws_credentials *credentials =
-      aws_credentials_new_copy(allocator, source);
+    struct aws_credentials *credentials = aws_credentials_new_copy(allocator, source);
 
-  // Verify string equality and pointer inequality
-  ASSERT_TRUE(aws_string_compare(credentials->access_key_id,
-                                 s_access_key_id_test_value) == 0);
-  ASSERT_TRUE(credentials->access_key_id != source->access_key_id);
+    // Verify string equality and pointer inequality
+    ASSERT_TRUE(aws_string_compare(credentials->access_key_id, s_access_key_id_test_value) == 0);
+    ASSERT_TRUE(credentials->access_key_id != source->access_key_id);
 
-  ASSERT_TRUE(aws_string_compare(credentials->secret_access_key,
-                                 s_secret_access_key_test_value) == 0);
-  ASSERT_TRUE(credentials->secret_access_key != source->secret_access_key);
+    ASSERT_TRUE(aws_string_compare(credentials->secret_access_key, s_secret_access_key_test_value) == 0);
+    ASSERT_TRUE(credentials->secret_access_key != source->secret_access_key);
 
-  ASSERT_TRUE(aws_string_compare(credentials->session_token,
-                                 s_session_token_test_value) == 0);
-  ASSERT_TRUE(credentials->session_token != source->session_token);
+    ASSERT_TRUE(aws_string_compare(credentials->session_token, s_session_token_test_value) == 0);
+    ASSERT_TRUE(credentials->session_token != source->session_token);
 
-  aws_credentials_destroy(credentials);
-  aws_credentials_destroy(source);
+    aws_credentials_destroy(credentials);
+    aws_credentials_destroy(source);
 
-  return 0;
+    return 0;
 }
 
 AWS_TEST_CASE(credentials_copy_test, s_credentials_copy_test);
