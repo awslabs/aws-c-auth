a632242f2b78e926d6d6f77565e539c7e0fb0e26
Bret Ambrose <bambrose@amazon.com>
2019-04-02 12:13:26 -0700

    More comments

--- tests/credentials_tests.c
+++ tests/credentials_tests.c
@@ -75,6 +75,10 @@ static int s_credentials_copy_test(struct aws_allocator *allocator, void *ctx) {
 
 AWS_TEST_CASE(credentials_copy_test, s_credentials_copy_test);
 
+/*
+ * Helper function that takes a provider, expected results from a credentials query,
+ * and uses the provider testing utils to query the results
+ */
 static int s_do_basic_provider_test(
     struct aws_credentials_provider *provider,
     int expected_calls,
@@ -176,14 +180,22 @@ static int s_do_environment_credentials_provider_failure(struct aws_allocator *a
     return 0;
 }
 
+/*
+ * Set of related tests that all check and make sure that if you don't specify enough
+ * of the credentials data in the environment, you get nothing when you query an
+ * environment provider.
+ */
 static int s_environment_credentials_provider_negative_test(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
 
+    /* nothing in the environment */
     ASSERT_TRUE(s_do_environment_credentials_provider_failure(allocator) == 0);
 
+    /* access key only shouldn't work */
     aws_set_environment_value(s_access_key_id_env_var, s_access_key_id_test_value);
     ASSERT_TRUE(s_do_environment_credentials_provider_failure(allocator) == 0);
 
+    /* secret key only shouldn't work either */
     aws_unset_environment_value(s_access_key_id_env_var);
     aws_set_environment_value(s_secret_access_key_env_var, s_secret_access_key_test_value);
     ASSERT_TRUE(s_do_environment_credentials_provider_failure(allocator) == 0);
