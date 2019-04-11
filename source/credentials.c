abb01aee76fd90c86b6aabc89eb1f0440540acb7
Bret Ambrose <bretambrose@gmail.com>
2019-04-11 10:49:09 -0700

    Static and Environment providers (#4)
    * Static and Environment providers

--- source/credentials.c
+++ source/credentials.c
@@ -15,7 +15,9 @@
 
 #include <aws/auth/credentials.h>
 
+#include <aws/common/environment.h>
 #include <aws/common/string.h>
+#include <aws/io/logging.h>
 
 struct aws_credentials *aws_credentials_new(
     struct aws_allocator *allocator,
@@ -68,4 +70,162 @@ void aws_credentials_destroy(struct aws_credentials *credentials) {
     }
 
     aws_mem_release(credentials->allocator, credentials);
+}
+
+/*
+ * provider API via vtable
+ */
+void aws_credentials_provider_destroy(struct aws_credentials_provider *provider) {
+    assert(provider->vtable->clean_up);
+
+    provider->vtable->clean_up(provider);
+
+    aws_mem_release(provider->allocator, provider);
+}
+
+int aws_credentials_provider_get_credentials(
+    struct aws_credentials_provider *provider,
+    aws_on_get_credentials_callback_fn callback,
+    void *user_data) {
+
+    assert(provider->vtable->get_credentials);
+
+    return provider->vtable->get_credentials(provider, callback, user_data);
+}
+
+/*
+ * Static provider implementation
+ *
+ * Just stuff the credentials in the impl member, and don't bother wrapping them
+ */
+static int s_static_credentials_provider_get_credentials_async(
+    struct aws_credentials_provider *provider,
+    aws_on_get_credentials_callback_fn callback,
+    void *user_data) {
+
+    struct aws_credentials *credentials = (struct aws_credentials *)provider->impl;
+
+    AWS_LOGF_INFO(
+        AWS_LS_AUTH_CREDENTIALS_PROVIDER,
+        "Static credentials provider (%p) successfully sourced credentials",
+        (void *)provider);
+    callback(credentials, user_data);
+
+    return AWS_OP_SUCCESS;
+}
+
+static void s_static_credentials_provider_clean_up(struct aws_credentials_provider *provider) {
+    struct aws_credentials *credentials = (struct aws_credentials *)provider->impl;
+
+    if (credentials != NULL) {
+        aws_credentials_destroy(credentials);
+    }
+}
+
+static struct aws_credentials_provider_vtable s_aws_credentials_provider_static_vtable = {
+    .get_credentials = s_static_credentials_provider_get_credentials_async,
+    .clean_up = s_static_credentials_provider_clean_up};
+
+struct aws_credentials_provider *aws_credentials_provider_static_new(
+    struct aws_allocator *allocator,
+    const struct aws_string *access_key_id,
+    const struct aws_string *secret_access_key,
+    const struct aws_string *session_token) {
+
+    struct aws_credentials_provider *provider =
+        (struct aws_credentials_provider *)aws_mem_acquire(allocator, sizeof(struct aws_credentials_provider));
+    if (provider == NULL) {
+        return NULL;
+    }
+
+    AWS_ZERO_STRUCT(*provider);
+
+    struct aws_credentials *credentials =
+        aws_credentials_new(allocator, access_key_id, secret_access_key, session_token);
+    if (credentials == NULL) {
+        goto on_new_credentials_failure;
+    }
+
+    provider->allocator = allocator;
+    provider->vtable = &s_aws_credentials_provider_static_vtable;
+    provider->impl = credentials;
+
+    return provider;
+
+on_new_credentials_failure:
+    aws_mem_release(allocator, provider);
+
+    return NULL;
+}
+
+/*
+ *  Environment provider implementation.
+ */
+AWS_STATIC_STRING_FROM_LITERAL(s_access_key_id_env_var, "AWS_ACCESS_KEY_ID");
+AWS_STATIC_STRING_FROM_LITERAL(s_secret_access_key_env_var, "AWS_SECRET_ACCESS_KEY");
+AWS_STATIC_STRING_FROM_LITERAL(s_session_token_env_var, "AWS_SESSION_TOKEN");
+
+static int s_credentials_provider_environment_get_credentials_async(
+    struct aws_credentials_provider *provider,
+    aws_on_get_credentials_callback_fn callback,
+    void *user_data) {
+
+    struct aws_allocator *allocator = provider->allocator;
+
+    struct aws_credentials *credentials =
+        (struct aws_credentials *)aws_mem_acquire(allocator, sizeof(struct aws_credentials));
+    if (credentials == NULL) {
+        callback(NULL, user_data);
+        return AWS_OP_ERR;
+    }
+
+    AWS_ZERO_STRUCT(*credentials);
+    credentials->allocator = allocator;
+
+    if (aws_get_environment_value(allocator, s_access_key_id_env_var, &credentials->access_key_id) != 0 ||
+        aws_get_environment_value(allocator, s_secret_access_key_env_var, &credentials->secret_access_key) != 0 ||
+        credentials->access_key_id == NULL || credentials->secret_access_key == NULL) {
+        AWS_LOGF_INFO(
+            AWS_LS_AUTH_CREDENTIALS_PROVIDER,
+            "Environment credentials provider (%p) was unable to source credentials",
+            (void *)provider);
+        callback(NULL, user_data);
+    } else {
+        AWS_LOGF_INFO(
+            AWS_LS_AUTH_CREDENTIALS_PROVIDER,
+            "Environment credentials provider (%p) successfully sourced credentials",
+            (void *)provider);
+        aws_get_environment_value(allocator, s_session_token_env_var, &credentials->session_token);
+        callback(credentials, user_data);
+    }
+
+    if (credentials != NULL) {
+        aws_credentials_destroy(credentials);
+    }
+
+    return AWS_OP_SUCCESS;
+}
+
+static void s_credentials_provider_environment_clean_up(struct aws_credentials_provider *provider) {
+    (void)provider;
+}
+
+static struct aws_credentials_provider_vtable s_aws_credentials_provider_environment_vtable = {
+    .get_credentials = s_credentials_provider_environment_get_credentials_async,
+    .clean_up = s_credentials_provider_environment_clean_up};
+
+struct aws_credentials_provider *aws_credentials_provider_new_environment(struct aws_allocator *allocator) {
+    struct aws_credentials_provider *provider =
+        (struct aws_credentials_provider *)aws_mem_acquire(allocator, sizeof(struct aws_credentials_provider));
+    if (provider == NULL) {
+        return NULL;
+    }
+
+    AWS_ZERO_STRUCT(*provider);
+
+    provider->allocator = allocator;
+    provider->vtable = &s_aws_credentials_provider_environment_vtable;
+    provider->impl = NULL;
+
+    return provider;
 }
---
+++ tests/credentials_provider_utils.h
@@ -0,0 +1,106 @@
+#ifndef AWS_AUTH_CREDENTIALS_PROVIDER_MOCK_H
+#define AWS_AUTH_CREDENTIALS_PROVIDER_MOCK_H
+
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/auth/private/aws_profile.h>
+
+#include <aws/common/condition_variable.h>
+#include <aws/common/mutex.h>
+
+struct aws_credentials;
+struct aws_credentials_provider;
+struct aws_string;
+
+/*
+ * This file contains a number of helper functions and data structures
+ * that let us verify async behavior within the credentials provider.
+ *
+ * It includes multiple provider mocks (one synchronous, one background-thread
+ * based and externally controllable), a synchronizing controller that uses
+ * concurrency primitives to ensure we can perform operations at troublesome
+ * time points (freeze the cached background query so that we can queue up
+ * multiple pending queries, for example), and misc supporting functions like
+ * time function mocks.
+ */
+
+/*
+ * test helper struct to correctly wait on async credentials callbacks
+ */
+struct aws_get_credentials_test_callback_result {
+    struct aws_mutex sync;
+    struct aws_condition_variable signal;
+    struct aws_credentials *credentials;
+    int count;
+    int required_count;
+};
+
+void aws_get_credentials_test_callback_result_init(
+    struct aws_get_credentials_test_callback_result *result,
+    int required_count);
+void aws_get_credentials_test_callback_result_clean_up(struct aws_get_credentials_test_callback_result *result);
+
+void aws_wait_on_credentials_callback(struct aws_get_credentials_test_callback_result *result);
+
+void aws_test_get_credentials_async_callback(struct aws_credentials *credentials, void *user_data);
+
+struct get_credentials_mock_result {
+    int error_code;
+    struct aws_credentials *credentials;
+};
+
+/*
+ * Mock credentials provider, synchronous
+ */
+struct aws_credentials_provider *aws_credentials_provider_new_mock(
+    struct aws_allocator *allocator,
+    struct get_credentials_mock_result *results,
+    size_t result_count);
+
+/*
+ * Credentials provider that puts a mock provider in a background thread and uses signalling to control callback
+ * invocation.  Useful to properly test query queuing during expiration
+ */
+struct aws_credentials_provider_mock_async_controller {
+    struct aws_mutex sync;
+    struct aws_condition_variable signal;
+    bool should_fire_callback;
+    bool should_quit;
+};
+
+void aws_credentials_provider_mock_async_controller_init(
+    struct aws_credentials_provider_mock_async_controller *controller);
+void aws_credentials_provider_mock_async_controller_clean_up(
+    struct aws_credentials_provider_mock_async_controller *controller);
+
+struct aws_credentials_provider *aws_credentials_provider_new_mock_async(
+    struct aws_allocator *allocator,
+    struct get_credentials_mock_result *results,
+    size_t result_count,
+    struct aws_credentials_provider_mock_async_controller *controller);
+
+/*
+ * Simple global clock mock
+ */
+int mock_aws_get_time(uint64_t *current_time);
+void mock_aws_set_time(uint64_t current_time);
+
+/*
+ * Credentials provider that always returns NULL.  Useful for chain tests.
+ */
+struct aws_credentials_provider *aws_credentials_provider_new_null(struct aws_allocator *allocator);
+
+#endif /* AWS_AUTH_CREDENTIALS_PROVIDER_MOCK_H */
