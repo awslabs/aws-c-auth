66a6a19de6fb941a87a97a7c9f42e6660f1317c8
Andrew Tang <tangliang551@gmail.com>
2020-03-18 22:41:36 -0700

    Change unsigned char to uint8_t

--- source/credentials_provider_imds.c
+++ source/credentials_provider_imds.c
@@ -390,7 +390,7 @@ static void s_imds_query_token(struct aws_credentials_provider_imds_user_data *a
 static void s_imds_query_instance_role_name(struct aws_credentials_provider_imds_user_data *imds_user_data);
 static void s_imds_query_instance_role_credentials(struct aws_credentials_provider_imds_user_data *imds_user_data);
 
-static bool s_isspace(unsigned char c) {
+static bool s_isspace(uint8_t c) {
     return isspace((int)c) != 0;
 }
 
