e68172b241a71b872cebbdc70b6e797a2f08de6c
Ryan Carper <51676630+rccarper@users.noreply.github.com>
2020-08-04 10:23:53 -0400

    Moving XML Parser out of aws-c-auth and into aws-c-common (#96)
    * Moving XML Parser out of aws-c-auth and into aws-c-common
    
    * Adapting to changed interface for xml_parser in aws-c-common

--- source/credentials_provider_sts.c
+++ source/credentials_provider_sts.c
@@ -4,11 +4,11 @@
  */
 #include <aws/auth/credentials.h>
 #include <aws/auth/private/credentials_utils.h>
-#include <aws/auth/private/xml_parser.h>
 #include <aws/auth/signable.h>
 #include <aws/auth/signing.h>
 #include <aws/auth/signing_config.h>
 #include <aws/auth/signing_result.h>
+#include <aws/common/xml_parser.h>
 
 #include <aws/common/clock.h>
 #include <aws/common/string.h>
@@ -221,16 +221,29 @@ static int s_on_incoming_body_fn(struct aws_http_stream *stream, const struct aw
 </AssumeRoleResponse>
  */
 static bool s_on_node_encountered_fn(struct aws_xml_parser *parser, struct aws_xml_node *node, void *user_data) {
-    if (aws_byte_cursor_eq_ignore_case(&node->name, &s_assume_role_root_name) ||
-        aws_byte_cursor_eq_ignore_case(&node->name, &s_assume_role_result_name) ||
-        aws_byte_cursor_eq_ignore_case(&node->name, &s_assume_role_credentials_name)) {
+
+    struct aws_byte_cursor node_name;
+    AWS_ZERO_STRUCT(node_name);
+
+    if (aws_xml_node_get_name(node, &node_name)) {
+        AWS_LOGF_ERROR(
+            AWS_LS_AUTH_CREDENTIALS_PROVIDER,
+            "(id=%p): While parsing credentials xml response for sts credentials provider, could not get xml node name "
+            "for function s_on_node_encountered_fn.",
+            user_data);
+        return false;
+    }
+
+    if (aws_byte_cursor_eq_ignore_case(&node_name, &s_assume_role_root_name) ||
+        aws_byte_cursor_eq_ignore_case(&node_name, &s_assume_role_result_name) ||
+        aws_byte_cursor_eq_ignore_case(&node_name, &s_assume_role_credentials_name)) {
         return aws_xml_node_traverse(parser, node, s_on_node_encountered_fn, user_data);
     }
 
     struct sts_creds_provider_user_data *provider_user_data = user_data;
     struct aws_byte_cursor credential_data;
     AWS_ZERO_STRUCT(credential_data);
-    if (aws_byte_cursor_eq_ignore_case(&node->name, &s_assume_role_access_key_id_name)) {
+    if (aws_byte_cursor_eq_ignore_case(&node_name, &s_assume_role_access_key_id_name)) {
         aws_xml_node_as_body(parser, node, &credential_data);
         provider_user_data->access_key_id =
             aws_string_new_from_array(provider_user_data->allocator, credential_data.ptr, credential_data.len);
@@ -244,13 +257,13 @@ static bool s_on_node_encountered_fn(struct aws_xml_parser *parser, struct aws_x
         }
     }
 
-    if (aws_byte_cursor_eq_ignore_case(&node->name, &s_assume_role_secret_key_name)) {
+    if (aws_byte_cursor_eq_ignore_case(&node_name, &s_assume_role_secret_key_name)) {
         aws_xml_node_as_body(parser, node, &credential_data);
         provider_user_data->secret_access_key =
             aws_string_new_from_array(provider_user_data->allocator, credential_data.ptr, credential_data.len);
     }
 
-    if (aws_byte_cursor_eq_ignore_case(&node->name, &s_assume_role_session_token_name)) {
+    if (aws_byte_cursor_eq_ignore_case(&node_name, &s_assume_role_session_token_name)) {
         aws_xml_node_as_body(parser, node, &credential_data);
         provider_user_data->session_token =
             aws_string_new_from_array(provider_user_data->allocator, credential_data.ptr, credential_data.len);
@@ -294,6 +307,7 @@ static void s_on_stream_complete_fn(struct aws_http_stream *stream, int error_co
     int http_response_code = 0;
     struct sts_creds_provider_user_data *provider_user_data = user_data;
     struct aws_credentials_provider_sts_impl *provider_impl = provider_user_data->provider->impl;
+    struct aws_xml_parser *xml_parser = NULL;
 
     provider_user_data->error_code = error_code;
 
@@ -360,10 +374,13 @@ static void s_on_stream_complete_fn(struct aws_http_stream *stream, int error_co
             goto finish;
         }
 
-        struct aws_xml_parser xml_parser;
-        struct aws_byte_cursor payload_cur = aws_byte_cursor_from_buf(&provider_user_data->output_buf);
+        struct aws_xml_parser_options options;
+        AWS_ZERO_STRUCT(options);
+        options.doc = aws_byte_cursor_from_buf(&provider_user_data->output_buf);
 
-        if (aws_xml_parser_init(&xml_parser, provider_user_data->provider->allocator, &payload_cur, 0)) {
+        xml_parser = aws_xml_parser_new(provider_user_data->provider->allocator, &options);
+
+        if (xml_parser == NULL) {
             goto finish;
         }
 
@@ -374,7 +391,7 @@ static void s_on_stream_complete_fn(struct aws_http_stream *stream, int error_co
 
         uint64_t now_seconds = aws_timestamp_convert(now, AWS_TIMESTAMP_NANOS, AWS_TIMESTAMP_SECS, NULL);
 
-        if (aws_xml_parser_parse(&xml_parser, s_on_node_encountered_fn, provider_user_data)) {
+        if (aws_xml_parser_parse(xml_parser, s_on_node_encountered_fn, provider_user_data)) {
             provider_user_data->error_code = aws_last_error();
             AWS_LOGF_ERROR(
                 AWS_LS_AUTH_CREDENTIALS_PROVIDER,
@@ -383,8 +400,6 @@ static void s_on_stream_complete_fn(struct aws_http_stream *stream, int error_co
                 aws_error_debug_str(provider_user_data->error_code));
             goto finish;
         }
-
-        aws_xml_parser_clean_up(&xml_parser);
 
         if (provider_user_data->access_key_id && provider_user_data->secret_access_key &&
             provider_user_data->session_token) {
@@ -404,6 +419,12 @@ static void s_on_stream_complete_fn(struct aws_http_stream *stream, int error_co
     }
 
 finish:
+
+    if (xml_parser != NULL) {
+        aws_xml_parser_destroy(xml_parser);
+        xml_parser = NULL;
+    }
+
     s_clean_up_user_data(provider_user_data);
 }
 
