5f3d03e8f72bb33e33d6f7872f3859ab59a2c2a4
Bret Ambrose <bretambrose@gmail.com>
2021-02-16 14:44:36 -0800

    Corrected comment

--- include/aws/auth/signable.h
+++ include/aws/auth/signable.h
@@ -151,7 +151,7 @@ AWS_AUTH_API extern const struct aws_string *g_aws_http_uri_property_name;
  *   (1) Header - hex encoding of the binary signature value
  *   (2) QueryParam - hex encoding of the binary signature value
  *   (3) Chunk/Sigv4 - hex encoding of the binary signature value
- *   (4) Chunk/Sigv4a - zero-padded hex encoding of the binary signature value
+ *   (4) Chunk/Sigv4a - fixed-size-rhs-padded (with '*') hex encoding of the binary signature value
  *   (5) Event - binary signature value (NYI)
  */
 AWS_AUTH_API extern const struct aws_string *g_aws_signature_property_name;
