3169e2d90dbf0edc6cd1be06e40dc0346ea7fd0a
Justin Boswell <boswej@amazon.com>
2021-10-11 14:34:37 -0700

    Ensure that aws-c-sdkutils is added as a dependency for downstream deps (#143)

--- cmake/aws-c-auth-config.cmake
+++ cmake/aws-c-auth-config.cmake
@@ -4,6 +4,7 @@ find_dependency(aws-c-common)
 find_dependency(aws-c-cal)
 find_dependency(aws-c-io)
 find_dependency(aws-c-http)
+find_dependency(aws-c-sdkutils)
 
 if (BUILD_SHARED_LIBS)
     include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
