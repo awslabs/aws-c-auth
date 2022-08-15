109bc3f9bbfb6593ec8415ebe6a5e8988a543cfb
Dengke Tang <dengket@amazon.com>
2022-08-15 15:25:52 -0700

    use the default load target function

--- cmake/aws-c-auth-config.cmake
+++ cmake/aws-c-auth-config.cmake
@@ -1,4 +1,5 @@
 include(CMakeFindDependencyMacro)
+include(AwsCrtLoadTarget)
 
 find_dependency(aws-c-common)
 find_dependency(aws-c-cal)
@@ -6,9 +7,4 @@ find_dependency(aws-c-io)
 find_dependency(aws-c-http)
 find_dependency(aws-c-sdkutils)
 
-if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
-else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
-endif()
-
+aws_load_target_default(@PROJECT_NAME@)
