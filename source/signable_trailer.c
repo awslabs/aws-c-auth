3e027005551af8d0bc55281e111618c95d8d9e60
Waqar Ahmed Khan <waahm7@gmail.com>
2023-02-01 08:25:47 -0800

    update comment

--- source/signable_trailer.c
+++ source/signable_trailer.c
@@ -106,7 +106,7 @@ struct aws_signable *aws_signable_new_trailing_headers(
     signable->impl = impl;
 
     /*
-     * Convert headers list to aws_signable_property_list_pair arraylist.
+     * Convert headers list to aws_signable_property_list_pair arraylist since they're not different types.
      */
     size_t header_count = aws_http_headers_count(trailing_headers);
     if (aws_array_list_init_dynamic(
