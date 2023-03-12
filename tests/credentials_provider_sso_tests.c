828cf2c406dbde5550f8005f32efbe4859523220
Waqar Ahmed Khan <waahm7@gmail.com>
2023-03-12 13:20:32 -0700

    cleanup tests

--- tests/credentials_provider_sso_tests.c
+++ tests/credentials_provider_sso_tests.c
@@ -27,75 +27,6 @@
 #include <aws/io/stream.h>
 #include <aws/io/tls_channel_handler.h>
 
-static bool received_callback = false;
-static struct aws_mutex lock;
-static struct aws_condition_variable tester_signal;
-static bool s_has_tester_received_credentials_callback(void *user_data) {
-    (void)user_data;
-
-    return received_callback;
-}
-static void s_get_credentials_callback(struct aws_credentials *credentials, int error_code, void *user_data) {
-    (void)user_data;
-    (void)credentials;
-    printf("credentials callback, %d", error_code);
-    AWS_LOGF_ERROR(AWS_LS_AUTH_CREDENTIALS_PROVIDER, "waahm7 callback %d", error_code);
-
-    received_callback = true;
-    AWS_FATAL_ASSERT(credentials != NULL);
-    aws_condition_variable_notify_one(&tester_signal);
-}
-
-static int s_credentials_provider_sso_new_destroy(struct aws_allocator *allocator, void *ctx) {
-    (void)ctx;
-    aws_auth_library_init(allocator);
-    struct aws_credentials_provider_sso_options options;
-    AWS_ZERO_STRUCT(options);
-    options.profile_name_override = aws_byte_cursor_from_c_str("AdministratorAccess-069542832437");
-
-    struct aws_tls_ctx_options tls_options;
-    aws_tls_ctx_options_init_default_client(&tls_options, allocator);
-    options.tls_ctx = aws_tls_client_ctx_new(allocator, &tls_options);
-
-    struct aws_event_loop_group *el_group = aws_event_loop_group_new_default(allocator, 0, NULL);
-
-    struct aws_host_resolver_default_options resolver_options = {
-        .el_group = el_group,
-        .max_entries = 8,
-    };
-    struct aws_host_resolver *resolver = aws_host_resolver_new_default(allocator, &resolver_options);
-
-    struct aws_client_bootstrap_options bootstrap_options = {
-        .event_loop_group = el_group,
-        .host_resolver = resolver,
-    };
-    options.bootstrap = aws_client_bootstrap_new(allocator, &bootstrap_options);
-
-    struct aws_credentials_provider *provider = aws_credentials_provider_new_sso(allocator, &options);
-    aws_credentials_provider_get_credentials(provider, s_get_credentials_callback, NULL);
-
-    if (aws_mutex_init(&lock)) {
-        return AWS_OP_ERR;
-    }
-
-    if (aws_condition_variable_init(&tester_signal)) {
-        return AWS_OP_ERR;
-    }
-
-    aws_condition_variable_wait_pred(&tester_signal, &lock, s_has_tester_received_credentials_callback, NULL);
-
-    AWS_LOGF_ERROR(AWS_LS_AUTH_CREDENTIALS_PROVIDER, "waahm7 releasing");
-
-    aws_credentials_provider_release(provider);
-    aws_event_loop_group_release(el_group);
-    aws_host_resolver_release(resolver);
-    aws_client_bootstrap_release(options.bootstrap);
-    aws_tls_ctx_release(options.tls_ctx);
-    aws_auth_library_clean_up();
-    return 0;
-}
-AWS_TEST_CASE(credentials_provider_sso_new_destroy, s_credentials_provider_sso_new_destroy);
-
 AWS_STATIC_STRING_FROM_LITERAL(s_sso_profile, "sso");
 static int s_aws_credentials_provider_sso_test_init_config_profile(
     struct aws_allocator *allocator,
@@ -137,90 +68,6 @@ AWS_STATIC_STRING_FROM_LITERAL(
     "[sso-session session]\n"
     "sso_start_url = https://d-123.awsapps.com/start\n"
     "sso_region = us-west-2\n");
-
-static int s_credentials_provider_sso_new_destroy_from_profile_config(struct aws_allocator *allocator, void *ctx) {
-    (void)ctx;
-
-    aws_credentials_provider_http_mock_tester_init(allocator);
-
-    struct aws_byte_buf content_buf;
-    struct aws_byte_buf existing_content = aws_byte_buf_from_c_str(aws_string_c_str(s_sso_profile_config_contents));
-    aws_byte_buf_init_copy(&content_buf, allocator, &existing_content);
-
-    struct aws_string *config_file_contents = aws_string_new_from_array(allocator, content_buf.buffer, content_buf.len);
-    ASSERT_TRUE(config_file_contents != NULL);
-    aws_byte_buf_clean_up(&content_buf);
-
-    s_aws_credentials_provider_sso_test_init_config_profile(allocator, config_file_contents);
-    aws_string_destroy(config_file_contents);
-
-    struct aws_credentials_provider_sso_options options = {
-        .bootstrap = credentials_provider_http_mock_tester.bootstrap,
-        .tls_ctx = credentials_provider_http_mock_tester.tls_ctx,
-        .function_table = &aws_credentials_provider_http_mock_function_table,
-        .shutdown_options =
-            {
-                .shutdown_callback = aws_credentials_provider_http_mock_on_shutdown_complete,
-                .shutdown_user_data = NULL,
-            },
-    };
-
-    struct aws_credentials_provider *provider = aws_credentials_provider_new_sso(allocator, &options);
-    ASSERT_NOT_NULL(provider);
-
-    aws_credentials_provider_release(provider);
-
-    aws_credentials_provider_http_mock_wait_for_shutdown_callback();
-
-    aws_credentials_provider_http_mock_tester_cleanup();
-
-    return 0;
-}
-AWS_TEST_CASE(
-    credentials_provider_sso_new_destroy_from_profile_config,
-    s_credentials_provider_sso_new_destroy_from_profile_config);
-
-static int s_credentials_provider_sso_new_destroy_from_sso_session_config(struct aws_allocator *allocator, void *ctx) {
-    (void)ctx;
-
-    aws_credentials_provider_http_mock_tester_init(allocator);
-
-    struct aws_byte_buf content_buf;
-    struct aws_byte_buf existing_content = aws_byte_buf_from_c_str(aws_string_c_str(s_sso_session_config_contents));
-    aws_byte_buf_init_copy(&content_buf, allocator, &existing_content);
-
-    struct aws_string *config_file_contents = aws_string_new_from_array(allocator, content_buf.buffer, content_buf.len);
-    ASSERT_TRUE(config_file_contents != NULL);
-    aws_byte_buf_clean_up(&content_buf);
-
-    s_aws_credentials_provider_sso_test_init_config_profile(allocator, config_file_contents);
-    aws_string_destroy(config_file_contents);
-
-    struct aws_credentials_provider_sso_options options = {
-        .bootstrap = credentials_provider_http_mock_tester.bootstrap,
-        .tls_ctx = credentials_provider_http_mock_tester.tls_ctx,
-        .function_table = &aws_credentials_provider_http_mock_function_table,
-        .shutdown_options =
-            {
-                .shutdown_callback = aws_credentials_provider_http_mock_on_shutdown_complete,
-                .shutdown_user_data = NULL,
-            },
-    };
-
-    struct aws_credentials_provider *provider = aws_credentials_provider_new_sso(allocator, &options);
-    ASSERT_NOT_NULL(provider);
-
-    aws_credentials_provider_release(provider);
-
-    aws_credentials_provider_http_mock_wait_for_shutdown_callback();
-
-    aws_credentials_provider_http_mock_tester_cleanup();
-
-    return 0;
-}
-AWS_TEST_CASE(
-    credentials_provider_sso_new_destroy_from_sso_session_config,
-    s_credentials_provider_sso_new_destroy_from_sso_session_config);
 
 static int s_credentials_provider_sso_failed_invalid_config(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
@@ -344,6 +191,77 @@ static int s_credentials_provider_sso_failed_invalid_config(struct aws_allocator
     return 0;
 }
 AWS_TEST_CASE(credentials_provider_sso_failed_invalid_config, s_credentials_provider_sso_failed_invalid_config);
+
+static int s_credentials_provider_sso_create_destroy_valid_config(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+
+    const struct {
+        const char *name;
+        const char *text;
+    } valid_config_examples[] = {
+
+        {"profile",
+         "[profile sso]\n"
+         "accessKey=access\n"
+         "sso_start_url=https://d-123.awsapps.com/start\n"
+         "sso_account_id=123\n"
+         "sso_region=us-west-2\n"
+         "sso_role_name=roleName\n"},
+
+        {"session",
+         "[profile sso]\n"
+         "accessKey=access\n"
+         "sso_account_id=123\n"
+         "sso_role_name=roleName\n"
+         "sso_session = session\n"
+         "[sso-session session]\n"
+         "sso_start_url = https://d-123.awsapps.com/start\n"
+         "sso_region = us-west-2\n"},
+
+        {"session with profile",
+         "[profile sso]\n"
+         "accessKey=access\n"
+         "sso_start_url=https://d-123.awsapps.com/start\n"
+         "sso_region=us-west-2\n"
+         "sso_account_id=123\n"
+         "sso_role_name=roleName\n"
+         "sso_session = session\n"
+         "[sso-session session]\n"
+         "sso_start_url = https://d-123.awsapps.com/start\n"
+         "sso_region = us-west-2\n"},
+
+    };
+
+    aws_credentials_provider_http_mock_tester_init(allocator);
+
+    struct aws_credentials_provider_sso_options options = {
+        .bootstrap = credentials_provider_http_mock_tester.bootstrap,
+        .tls_ctx = credentials_provider_http_mock_tester.tls_ctx,
+        .function_table = &aws_credentials_provider_http_mock_function_table,
+        .shutdown_options =
+            {
+                .shutdown_callback = aws_credentials_provider_http_mock_on_shutdown_complete,
+                .shutdown_user_data = NULL,
+            },
+    };
+    for (int i = 0; i < AWS_ARRAY_SIZE(valid_config_examples); i++) {
+        printf("valid config example [%d]: %s\n", i, valid_config_examples[i].name);
+        struct aws_string *content = aws_string_new_from_c_str(allocator, valid_config_examples[i].text);
+        ASSERT_TRUE(content != NULL);
+        s_aws_credentials_provider_sso_test_init_config_profile(allocator, content);
+        aws_string_destroy(content);
+        struct aws_credentials_provider *provider = aws_credentials_provider_new_sso(allocator, &options);
+        ASSERT_NOT_NULL(provider);
+        aws_credentials_provider_release(provider);
+    }
+
+    aws_credentials_provider_http_mock_tester_cleanup();
+
+    return 0;
+}
+AWS_TEST_CASE(
+    credentials_provider_sso_create_destroy_valid_config,
+    s_credentials_provider_sso_create_destroy_valid_config);
 
 AWS_STATIC_STRING_FROM_LITERAL(
     s_expected_sso_request_path,
@@ -520,6 +438,7 @@ static int s_credentials_provider_sso_request_failure(struct aws_allocator *allo
 
     aws_credentials_provider_http_mock_tester_init(allocator);
     credentials_provider_http_mock_tester.is_request_successful = false;
+    credentials_provider_http_mock_tester.response_code = AWS_HTTP_STATUS_CODE_400_BAD_REQUEST;
 
     struct aws_string *actual_home = aws_string_new_from_c_str(allocator, getenv("HOME"));
     /* redirect $HOME */
@@ -562,7 +481,7 @@ static int s_credentials_provider_sso_request_failure(struct aws_allocator *allo
 
     aws_credentials_provider_http_mock_wait_for_credentials_result();
 
-    ASSERT_SUCCESS(s_verify_credentials(true /*request made*/, false /*get creds*/, 4 /*expected attempts*/));
+    ASSERT_SUCCESS(s_verify_credentials(true /*request made*/, false /*get creds*/, 1 /*expected attempts*/));
 
     aws_credentials_provider_release(provider);
 
--- tests/token_provider_sso_tests.c
+++ tests/token_provider_sso_tests.c
@@ -25,17 +25,11 @@ struct sso_session_profile_example {
 static int s_sso_token_provider_profile_invalid_profile_test(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
 
-    static struct sso_session_profile_example s_invalid_profile_examples[] = {
+    const struct sso_session_profile_example invalid_profile_examples[] = {
         {
             .name = "No config",
             .text = AWS_BYTE_CUR_INIT_FROM_STRING_LITERAL(
                 "[profile default]\naws_access_key_id=fake_access_key\naws_secret_access_key=fake_secret_key\n"),
-        },
-        {
-            .name = "No sso-region",
-            .text = AWS_BYTE_CUR_INIT_FROM_STRING_LITERAL("[profile "
-                                                          "default]\naws_access_key_id=fake_access_key\naws_secret_"
-                                                          "access_key=fake_secret_key\nsso_start_url=url\n"),
         },
         {
             .name = "No sso_start_url",
@@ -52,17 +46,14 @@ static int s_sso_token_provider_profile_invalid_profile_test(struct aws_allocato
         },
     };
 
-    aws_unset_environment_value(s_default_profile_env_variable_name);
-    aws_unset_environment_value(s_default_config_path_env_variable_name);
-    aws_unset_environment_value(s_default_credentials_path_env_variable_name);
     struct aws_string *config_file_str = aws_create_process_unique_file_name(allocator);
     struct aws_token_provider_sso_profile_options options = {
         .config_file_name_override = aws_byte_cursor_from_string(config_file_str),
     };
 
-    for (size_t i = 0; i < AWS_ARRAY_SIZE(s_invalid_profile_examples); ++i) {
-        printf("invalid example [%zu]: %s\n", i, s_invalid_profile_examples[i].name);
-        struct aws_string *config_contents = aws_string_new_from_cursor(allocator, &s_invalid_profile_examples[i].text);
+    for (size_t i = 0; i < AWS_ARRAY_SIZE(invalid_profile_examples); ++i) {
+        printf("invalid example [%zu]: %s\n", i, invalid_profile_examples[i].name);
+        struct aws_string *config_contents = aws_string_new_from_cursor(allocator, &invalid_profile_examples[i].text);
         ASSERT_SUCCESS(aws_create_profile_file(config_file_str, config_contents));
         ASSERT_NULL(aws_token_provider_new_sso_profile(allocator, &options));
         aws_string_destroy(config_contents);
@@ -154,9 +145,6 @@ static int s_sso_token_provider_sso_session_invalid_profile_test(struct aws_allo
         },
     };
 
-    aws_unset_environment_value(s_default_profile_env_variable_name);
-    aws_unset_environment_value(s_default_config_path_env_variable_name);
-    aws_unset_environment_value(s_default_credentials_path_env_variable_name);
     struct aws_string *config_file_str = aws_create_process_unique_file_name(allocator);
     struct aws_token_provider_sso_session_options options = {
         .config_file_name_override = aws_byte_cursor_from_string(config_file_str),
@@ -190,14 +178,14 @@ static int s_sso_token_provider_sso_session_valid_profile_test(struct aws_alloca
                                                           "1\nsso_start_url=url"),
         },
         {
-            .name = "with nsso_region",
+            .name = "with sso_region",
             .text = AWS_BYTE_CUR_INIT_FROM_STRING_LITERAL("[default]\naws_access_key_id=fake_access_key\naws_secret_"
                                                           "access_key=fake_secret_key\nsso_region=us-east-1\nsso_"
                                                           "session=dev\n[sso-session dev]\nsso_region=us-east-"
                                                           "1\nsso_start_url=url"),
         },
         {
-            .name = "with nsso_start_url",
+            .name = "with sso_start_url",
             .text = AWS_BYTE_CUR_INIT_FROM_STRING_LITERAL("[default]\naws_access_key_id=fake_access_key\naws_secret_"
                                                           "access_key=fake_secret_key\nsso_start_url=url\nsso_"
                                                           "session=dev\n[sso-session dev]\nsso_region=us-east-"
@@ -213,9 +201,6 @@ static int s_sso_token_provider_sso_session_valid_profile_test(struct aws_alloca
         },
     };
 
-    aws_unset_environment_value(s_default_profile_env_variable_name);
-    aws_unset_environment_value(s_default_config_path_env_variable_name);
-    aws_unset_environment_value(s_default_credentials_path_env_variable_name);
     struct aws_string *config_file_str = aws_create_process_unique_file_name(allocator);
     struct aws_token_provider_sso_session_options options = {
         .config_file_name_override = aws_byte_cursor_from_string(config_file_str),
