288373b5b80a462f1a470bb11d4bd755a158da8b
Waqar Ahmed Khan <waahm7@gmail.com>
2023-04-03 15:10:08 -0700

    move token api to sso_token_utils

--- include/aws/auth/private/sso_token_utils.h
+++ include/aws/auth/private/sso_token_utils.h
@@ -30,6 +30,29 @@ void aws_sso_token_destroy(struct aws_sso_token *token);
 AWS_AUTH_API
 struct aws_sso_token *aws_sso_token_new_from_file(struct aws_allocator *allocator, const struct aws_string *file_path);
 
+/**
+ * Creates a set of AWS credentials based on a token with expiration.
+ *
+ * @param allocator memory allocator to use for all memory allocation
+ * @param token token for the credentials
+ * @param expiration_timepoint_in_seconds time at which these credentials expire
+ * @return a new pair of AWS credentials, or NULL
+ */
+AWS_AUTH_API
+struct aws_credentials *aws_credentials_new_token(
+    struct aws_allocator *allocator,
+    struct aws_byte_cursor token,
+    uint64_t expiration_timepoint_in_seconds);
+
+/**
+ * Get the token from a set of AWS credentials
+ *
+ * @param credentials credentials to get the token from
+ * @return a byte cursor to the token or an empty byte cursor if there is no token
+ */
+AWS_AUTH_API
+struct aws_byte_cursor aws_credentials_get_token(const struct aws_credentials *credentials);
+
 AWS_EXTERN_C_END
 
 #endif /* AWS_AUTH_TOKEN_PRIVATE_H */
