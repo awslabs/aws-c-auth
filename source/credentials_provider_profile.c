e178b9b54b668e7f2244918eeddaabc662d1b341
Waqar Ahmed Khan <waahm7@gmail.com>
2024-01-17 16:08:23 -0800

    undo

--- source/credentials_provider_profile.c
+++ source/credentials_provider_profile.c
@@ -1,7 +1,6 @@
 /**
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
- * SPDX-License-Identifier: Apache-2.0.
  */
 
 #include <aws/auth/credentials.h>
