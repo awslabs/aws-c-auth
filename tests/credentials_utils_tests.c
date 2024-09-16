88c33e150fedbd63f0f59c9bb19813791f0bc17a
DmitriyMusatkin <musatkd@amazon.com>
2024-09-16 14:37:41 -0700

    allocators

--- tests/credentials_utils_tests.c
+++ tests/credentials_utils_tests.c
@@ -16,42 +16,42 @@ static int s_credentials_utils_construct_endpoint_test(struct aws_allocator *all
 
     region = aws_string_new_from_c_str(allocator, "us-east-2");
     ASSERT_SUCCESS(
-        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, aws_region, service_name));
+        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, region, service_name));
     ASSERT_STR_EQUALS("sts.us-east-2.amazonaws.com", aws_string_c_str(endpoint));
     aws_string_destroy(endpoint);
     aws_string_destroy(region);
 
     region = aws_string_new_from_c_str(allocator, "cn-northwest-1");
     ASSERT_SUCCESS(
-        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, aws_region, service_name));
+        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, region, service_name));
     ASSERT_STR_EQUALS("sts.cn-northwest-1.amazonaws.com.cn", aws_string_c_str(endpoint));
     aws_string_destroy(endpoint);
     aws_string_destroy(region);
 
     region = aws_string_new_from_c_str(allocator, "us-iso-east-1");
     ASSERT_SUCCESS(
-        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, aws_region, service_name));
+        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, region, service_name));
     ASSERT_STR_EQUALS("sts.us-iso-east-1.c2s.is.gov", aws_string_c_str(endpoint));
     aws_string_destroy(endpoint);
     aws_string_destroy(region);
 
     region = aws_string_new_from_c_str(allocator, "us-isob-east-1");
     ASSERT_SUCCESS(
-        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, aws_region, service_name));
+        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, region, service_name));
     ASSERT_STR_EQUALS("sts.us-isob-east-1.sc2s.sgov.gov", aws_string_c_str(endpoint));
     aws_string_destroy(endpoint);
     aws_string_destroy(region);
 
     region = aws_string_new_from_c_str(allocator, "eu-isoe-west-1");
     ASSERT_SUCCESS(
-        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, aws_region, service_name));
+        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, region, service_name));
     ASSERT_STR_EQUALS("sts.eu-isoe-west-1.cloud.adc-e.uk", aws_string_c_str(endpoint));
     aws_string_destroy(endpoint);
     aws_string_destroy(region);
 
     region = aws_string_new_from_c_str(allocator, "us-isof-south-1");
     ASSERT_SUCCESS(
-        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, aws_region, service_name));
+        aws_credentials_provider_construct_regional_endpoint(allocator, &endpoint, region, service_name));
     ASSERT_STR_EQUALS("sts.us-isof-south-1.csp.hci.ic.gov", aws_string_c_str(endpoint));
     aws_string_destroy(endpoint);
     aws_string_destroy(region);
