3688a9728079d2e00c1142fca9b4bc84e46050dc
Waqar Ahmed Khan <waahm7@gmail.com>
2025-02-04 13:17:27 -0800

    add test for new endpoint overrides

--- tests/credentials_utils_tests.c
+++ tests/credentials_utils_tests.c
@@ -4,8 +4,9 @@
  */
 
 #include <aws/auth/private/credentials_utils.h>
-#include <aws/testing/aws_test_harness.h>
 #include <aws/common/environment.h>
+#include <aws/sdkutils/aws_profile.h>
+#include <aws/testing/aws_test_harness.h>
 
 static int s_credentials_utils_construct_endpoint_test(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
@@ -57,7 +58,23 @@ static int s_credentials_utils_construct_endpoint_test(struct aws_allocator *all
     aws_string_destroy(endpoint);
     aws_string_destroy(region);
 
-    region = aws_string_new_from_c_str(allocator, "us-east-2");
+    aws_string_destroy(service_name);
+    aws_string_destroy(service_name_env);
+
+    return 0;
+}
+
+AWS_TEST_CASE(credentials_utils_construct_endpoint_test, s_credentials_utils_construct_endpoint_test);
+
+static int s_credentials_utils_endpoint_override_test(struct aws_allocator *allocator, void *ctx) {
+
+
+    struct aws_string *region = aws_string_new_from_c_str(allocator, "us-east-2");
+    struct aws_string *service_name = aws_string_new_from_c_str(allocator, "sts");
+    struct aws_string *service_name_env = aws_string_new_from_c_str(allocator, "STS");
+    struct aws_string *endpoint = NULL;
+
+    /* test service-specific endpoint override */
     struct aws_string *endpoint_override_env = aws_string_new_from_c_str(allocator, "AWS_ENDPOINT_URL_STS");
     struct aws_string *endpoint_override = aws_string_new_from_c_str(allocator, "test.endpoint.override.com");
     aws_set_environment_value(endpoint_override_env, endpoint_override);
@@ -68,12 +85,71 @@ static int s_credentials_utils_construct_endpoint_test(struct aws_allocator *all
     aws_string_destroy(endpoint_override);
     aws_string_destroy(endpoint_override_env);
     aws_string_destroy(endpoint);
-    aws_string_destroy(region);
 
+    /* test global endpoint override */
+    struct aws_string *endpoint_override_env_global = aws_string_new_from_c_str(allocator, "AWS_ENDPOINT_URL");
+    endpoint_override = aws_string_new_from_c_str(allocator, "global.endpoint.override.com");
+    aws_set_environment_value(endpoint_override_env_global, endpoint_override);
+    ASSERT_SUCCESS(aws_credentials_provider_construct_regional_endpoint(
+        allocator, &endpoint, region, service_name, service_name_env, service_name, NULL, NULL));
+    ASSERT_STR_EQUALS(aws_string_c_str(endpoint_override), aws_string_c_str(endpoint));
+    aws_unset_environment_value(endpoint_override_env_global);
+    aws_string_destroy(endpoint_override);
+    aws_string_destroy(endpoint_override_env_global);
+    aws_string_destroy(endpoint);
+
+    /* test service-specific config endpoint override */
+    struct aws_byte_cursor service_override_config_contents =
+        aws_byte_cursor_from_c_str("[profile test-profile]\n"
+                                   "test = 123\n"
+                                   "services = test-services\n"
+                                   "[services test-services]\n"
+                                   "sts =\n"
+                                   "    endpoint_url = test.sts.endpoint.com\n"
+                                   "[sso-session session]\n");
+    struct aws_byte_buf config_file;
+    AWS_ZERO_STRUCT(config_file);
+    aws_byte_buf_init_copy_from_cursor(&config_file, allocator, service_override_config_contents);
+    struct aws_profile_collection *profile_collection =
+        aws_profile_collection_new_from_buffer(allocator, &config_file, AWS_PST_CONFIG);
+    struct aws_string *profile_name = aws_string_new_from_c_str(allocator, "test-profile");
+    const struct aws_profile *profile = aws_profile_collection_get_profile(profile_collection, profile_name);
+
+    struct aws_string *expected_endpoint = aws_string_new_from_c_str(allocator, "test.sts.endpoint.com");
+
+    ASSERT_SUCCESS(aws_credentials_provider_construct_regional_endpoint(
+        allocator, &endpoint, region, service_name, service_name, service_name, profile_collection, profile));
+    ASSERT_STR_EQUALS(aws_string_c_str(expected_endpoint), aws_string_c_str(endpoint));
+    aws_string_destroy(expected_endpoint);
+    aws_byte_buf_clean_up(&config_file);
+    aws_profile_collection_release(profile_collection);
+    aws_string_destroy(endpoint);
+    aws_string_destroy(profile_name);
+    /* test global config endpoint override */
+    struct aws_byte_cursor global_override_config_contents =
+        aws_byte_cursor_from_c_str("[profile test-profile]\n"
+                                   "test = 123\n"
+                                   "endpoint_url = global.sts.endpoint.com\n");
+    AWS_ZERO_STRUCT(config_file);
+    aws_byte_buf_init_copy_from_cursor(&config_file, allocator, global_override_config_contents);
+    profile_collection =
+        aws_profile_collection_new_from_buffer(allocator, &config_file, AWS_PST_CONFIG);
+    profile_name = aws_string_new_from_c_str(allocator, "test-profile");
+    profile = aws_profile_collection_get_profile(profile_collection, profile_name);
+    expected_endpoint = aws_string_new_from_c_str(allocator, "global.sts.endpoint.com");
+
+    ASSERT_SUCCESS(aws_credentials_provider_construct_regional_endpoint(
+        allocator, &endpoint, region, service_name, service_name, service_name, profile_collection, profile));
+    ASSERT_STR_EQUALS(aws_string_c_str(expected_endpoint), aws_string_c_str(endpoint));
+    aws_string_destroy(expected_endpoint);
+    aws_byte_buf_clean_up(&config_file);
+    aws_profile_collection_release(profile_collection);
+    aws_string_destroy(endpoint);
+    aws_string_destroy(profile_name);
+
+    aws_string_destroy(region);
     aws_string_destroy(service_name);
     aws_string_destroy(service_name_env);
-
-    return 0;
+    return AWS_OP_SUCCESS;
 }
-
-AWS_TEST_CASE(credentials_utils_construct_endpoint_test, s_credentials_utils_construct_endpoint_test);
+AWS_TEST_CASE(credentials_utils_endpoint_override_test, s_credentials_utils_endpoint_override_test);
